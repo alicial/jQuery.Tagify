50d5fb6964262088698c1a631890ed31c01e2f3c
unknown <michael@.(none)>
2012-03-03 22:38:58 +0100

    Add blur event support to create tags, when addTagOnBlur setting is true

--- demo/jquery.tagify.js
+++ demo/jquery.tagify.js
@@ -7,7 +7,8 @@
 			delimiters: [13, 188],          // what user can type to complete a tag in char codes: [enter], [comma]
 			outputDelimiter: ',',           // delimiter for tags in original input field
 			cssClass: 'tagify-container',   // CSS class to style the tagify div and tags, see stylesheet
-			addTagPrompt: 'add tags'        // placeholder text
+			addTagPrompt: 'add tags',       // placeholder text
+			addTagOnBlur: true				// Add a tag on blur when not empty
 		},
 		
 		_create: function() {
@@ -50,6 +51,21 @@
 							return false;
 						}
 						return;
+					}
+				})
+				// When needed, add tags on blur
+				.blur( function(e) {
+					var $this = $(this);
+					
+					if ( false === opts.addTagOnBlur ) {
+						return false;
+					}
+					
+					// if lose focus on input field, check if length is empty
+					if ('' !== $this.val()) {
+						self.add( $this.val() );
+						e.preventDefault(); 
+						return false;
 					}
 				});
 				
